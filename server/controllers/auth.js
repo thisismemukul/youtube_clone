031a8da3b46fb1281dcefd03391d2da3bc8f30cc
Mukul Saini <thisismemukul@gmail.com>
2022-07-27 17:20:03 +0530

    Saving User to DB with Password Hash

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 import User from '../models/User.js';
+import bcrypt from "bcryptjs";
+
 export const signup = async(req, res) => {
-    console.log(req.body);
-    // try {
-    //     const newUser = await User(req.body);
-    // } catch (error) {
-    //     //todo        
-    // }
+    try {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(req.body.password, salt);
+        const newUser = await User({...req.body, password: hash });
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        //todo        
+    }
 }
