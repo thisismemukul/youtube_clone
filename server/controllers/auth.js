326a2843287f554d3769254bd822ec90b41726b5
Mukul Saini <thisismemukul@gmail.com>
2022-07-28 14:25:55 +0530

    Auth SignIn SignUp Completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import User from '../models/User.js';
 import bcrypt from "bcryptjs";
 import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next) => {
     try {
@@ -16,10 +17,16 @@ export const signup = async(req, res, next) => {
 }
 export const signin = async(req, res, next) => {
     try {
-        const user = await User.findOne({ username: req.body.username } || { email: req.body.email });
+        const user = await User.findOne(req.body.username ? { username: req.body.username } : { email: req.body.email });
         if (!user) return next(createError(404, "User not found"));
         const isCorrect = await bcrypt.compare(req.body.password, user.password);
         if (!isCorrect) return next(createError(400, "Wrong Credentials"));
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const { password, ...userData } = user._doc;
+        res.cookie("access_token", token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        }).status(200).json(userData);
     } catch (err) {
         next(err);
     }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
@@ -10,13 +11,17 @@ import commentRoutes from "./routes/comments.js";
 const app = express();
 dotenv.config();
 const connect = () => {
-    mongoose.connect(process.env.MONGO).then(() => {
+    mongoose.connect(process.env.MONGO, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }).then(() => {
         console.log("connected to mongo");
     }).catch(err => {
         console.log(err);
     });
 }
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
