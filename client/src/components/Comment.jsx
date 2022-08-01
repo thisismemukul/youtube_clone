feb15e4e69d8154d31b4d5fd3a34ba3cec04b649
Mukul Saini <thisismemukul@gmail.com>
2022-08-01 16:22:41 +0530

    Comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,52 +1,63 @@
-import React from 'react';
-import styled from 'styled-components';
-import { SPACING, SIZES } from '../constants';
-const Container = styled.div`
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
   display: flex;
-  gap: ${SPACING.s}px;
-  margin-bottom: ${SIZES.extremeLarge}px;
+  gap: 10px;
+  margin: 30px 0px;
 `;
+
 const Avatar = styled.img`
-width: 36px;
-height: 36px;
-border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
 `;
+
 const Details = styled.div`
-display: flex;
-flex-direction: column;
-gap: ${SPACING.s}px;
-color: ${({ theme }) => theme.text};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: ${({ theme }) => theme.text};
 `;
 const Name = styled.span`
-font-size: ${SIZES.font}px;
-font-weight: 500;
+  font-size: 13px;
+  font-weight: 500;
 `;
+
 const Date = styled.span`
-font-size: ${SIZES.small}px;
-font-weight: 400;
-color: ${({ theme }) => theme.textSoft};
-margin-left: ${SPACING.xs}px;
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.textSoft};
+  margin-left: 5px;
 `;
+
 const Text = styled.span`
-font-size: ${SIZES.body}px;
+  font-size: 14px;
 `;
-const Comment = () => {
+
+const Comment = ({ comment }) => {
+  const [channel, setChannel] = useState({});
+
+  useEffect(() => {
+    const fetchComment = async () => {
+      const res = await axios.get(`/users/find/${comment.userId}`);
+      setChannel(res.data)
+    };
+    fetchComment();
+  }, [comment.userId]);
+
   return (
     <Container>
-      <Avatar src='https://yt3.ggpht.com/k-_HJtdmP1EiG-ypRyZygN6UmyA9lpI0mR-BBkiDADVuGTAYSnMe4Wz6gv635JGeZWoBHrzXZpc=s88-c-k-c0x00ffffff-no-rj-mo' />
+      <Avatar src={channel.img} />
       <Details>
-        <Name>Jonh Doe  <Date>2 hours ago</Date></Name>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Pellentesque euismod, nisi eu consectetur consectetur,
-          nisl nisi consectetur nisl, eu consectetur nisl nisi euismod,
-          nisl nisi consectetur nisl, eu consectetur nisl nisi euismod,
-          nisl nisi consectetur nisl, eu consectetur nisl nisi euismod,
-        </Text>
+        <Name>
+          {channel.name} <Date>1 day ago</Date>
+        </Name>
+        <Text>{comment.desc}</Text>
       </Details>
     </Container>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
