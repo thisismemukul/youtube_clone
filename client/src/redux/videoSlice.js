1738ae6e0d8f3a17b6e7f6c52564e5e16688ee30
Mukul Saini <thisismemukul@gmail.com>
2022-08-01 15:03:35 +0530

    React Fetch Data with Redux Toolkit

--- client/src/redux/videoSlice.js
+++ client/src/redux/videoSlice.js
@@ -1,33 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser: null,
+    currentVideo: null,
     loading: false,
     error: false,
-}
+};
 
 export const videoSlice = createSlice({
-    name: 'user',
+    name: "video",
     initialState,
     reducers: {
-        loginStart: (state) => {
-            state.loading = true
+        fetchStart: (state) => {
+            state.loading = true;
         },
-        loginSuccess: (state, action) => {
-            state.loading = false
-            state.currentUser = action.payload
+        fetchSuccess: (state, action) => {
+            state.loading = false;
+            state.currentVideo = action.payload;
         },
-        loginFailure: (state) => {
-            state.loading = false
-            state.error = true
+        fetchFailure: (state) => {
+            state.loading = false;
+            state.error = true;
         },
-        logout: (state) => {
-            return initialState;
-        }
+        like: (state, action) => {
+            if (!state.currentVideo.likes.includes(action.payload)) {
+                state.currentVideo.likes.push(action.payload);
+                state.currentVideo.dislikes.splice(
+                    state.currentVideo.dislikes.findIndex(
+                        (userId) => userId === action.payload
+                    ),
+                    1
+                );
+            }
+        },
+        dislike: (state, action) => {
+            if (!state.currentVideo.dislikes.includes(action.payload)) {
+                state.currentVideo.dislikes.push(action.payload);
+                state.currentVideo.likes.splice(
+                    state.currentVideo.likes.findIndex(
+                        (userId) => userId === action.payload
+                    ),
+                    1
+                );
+            }
+        },
     },
-})
+});
 
-// Action creators are generated for each case reducer function
-export const { loginStart, loginSuccess, loginFailure, logout } = videoSlice.actions
+export const { fetchStart, fetchSuccess, fetchFailure, like, dislike } =
+videoSlice.actions;
 
-export default videoSlice.reducer
+export default videoSlice.reducer;
