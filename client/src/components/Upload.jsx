0d299304290823d943a86ea48cde6a4b02f9ae7e
Mukul Saini <thisismemukul@gmail.com>
2022-08-02 11:54:13 +0530

    upload video / image to firebase

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,6 +1,9 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { SPACING } from '../constants';
 import styled from 'styled-components';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from '../firebase';
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -68,32 +71,86 @@ const Upload = ({ setOpen }) => {
   const [imgPer, setImgPer] = useState(0);
   const [video, setVideo] = useState(undefined);
   const [videoPer, setVideoPer] = useState(0);
-  const [desc, setDesc] = useState("");
-  const [title, setTitle] = useState("");
+  const [inputs, setInputs] = useState({});
   const [tags, setTags] = useState([]);
+
+  const handleChange = (e) => {
+    setInputs(prev => {
+      return { ...prev, [e.target.name]: e.target.value }
+    });
+  }
 
   const handleTags = (e) => {
     setTags(e.target.value.split(","));
   }
-  useEffect(() => {  }, [video]);
-  useEffect(() => {  }, [img]);
-  
-    return (
-        <Container>
-            <Wrapper>
-                <Close onClick={()=>setOpen(false)}>X</Close>
-                <Title>Upload a New Video</Title>
-                <Label>Video</Label>
-                <Input type="file" accept="video/*" onChange={e=>setVideo(e.target.files[0])} />
-                <Input type="text" placeholder="Title" onChange={e=>setTitle(e.target.value)} />
-                <Desc placeholder="Description" rows={8} onChange={e=>setDesc(e.target.value)} />
-                <Input type="text" placeholder="Separate the tags with commas." onChange={handleTags} />
-                <Label>Image</Label>
-                <Input type="file" accept="image/*" onChange={e=>setImg(e.target.files[0])} />
-                <Button>Upload</Button>
-            </Wrapper>
-        </Container>
-    )
+
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl" ? setImgPer(Math.round(progress)) : setVideoPer(Math.round(progress));
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => { },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setInputs(prev => {
+            return { ...prev, [urlType]: downloadURL }
+          });
+        });
+      }
+    );
+
+  };
+  useEffect(() => {
+    video && uploadFile(video, "videoUrl");
+  }, [video]);
+  useEffect(() => {
+    img && uploadFile(img, "imgUrl");
+  }, [img]);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a New Video</Title>
+        <Label>Video</Label>
+        {videoPer ? (
+          "Uploading " + videoPer + "%"
+        ) :
+          (<Input type="file" accept="video/*" onChange={e => setVideo(e.target.files[0])} />)
+        }
+        <Input type="text" placeholder="Title" name="title" onChange={handleChange} />
+        <Desc placeholder="Description" rows={8} name="desc" onChange={handleChange} />
+        <Input type="text" placeholder="Separate the tags with commas." onChange={handleTags} />
+        <Label>Image</Label>
+        {imgPer ? (
+          "Uploading " + imgPer + "%"
+        ) :
+          (<Input type="file" accept="image/*" onChange={e => setImg(e.target.files[0])} />)
+        }
+        <Button>Upload</Button>
+      </Wrapper>
+    </Container>
+  )
 }
 
 export default Upload
