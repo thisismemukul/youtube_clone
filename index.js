d2dc88c8070d0a93c57ec3b226d8d3fdcb2c4ca4
Mukul Saini <thisismemukul@gmail.com>
2022-08-13 17:48:39 +0530

    vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import path from "path";
+// import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.js";
@@ -43,10 +43,11 @@ app.use((err, req, res, next) => {
 // serve static assets in production
 if (process.env.NODE_ENV === "production") {
     // set static folder
-    app.use(express.static("client/build"));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    app.use(express.static("./client/build"));
+    // app.get('/', (req, res) => {
+    //     app.use(express.static(path.resolve(__dirname, './client', './build')))
+    //     res.sendFile(path.resolve(__dirname, './client', './build', './index.html'))
+    // })
 }
 
 const PORT = process.env.PORT || 8000;
