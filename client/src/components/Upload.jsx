ef4ebde2af6f4273f8bd648f37146f5ab76b5fae
Mukul Saini <thisismemukul@gmail.com>
2022-08-13 17:23:03 +0530

    folder structure

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -134,7 +134,7 @@ const Upload = ({ setOpen }) => {
 
   const handleUpload = async (e) => {
     e.preventDefault();
-    const res = await axios.post('/videos', { ...inputs, tags });
+    const res = await axios.post(`/videos`, { ...inputs, tags });
     setOpen(false);
     res.status === 200 && navigate(`/video/${res.data._id}`);
   }
