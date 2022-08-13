69aa3f11abb0bd4dbed378bd8f736ae7a07b2886
Mukul Saini <thisismemukul@gmail.com>
2022-08-13 15:11:26 +0530

    Reday for deploy v1

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
 // serve static assets in production
 if (process.env.NODE_ENV === "production") {
     // set static folder
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "client/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
